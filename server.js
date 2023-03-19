f1378a808dd7cde6dce9ce6f83040ee9e4a95ea3
capricia <capriciaw16@gmail.com>
2023-03-18 23:16:53 -0500

    updstylebootstrap

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
-const express = require('express')
-const { engine } = ('express-handlerbars')
-const exphbs = require('express-handlebars')
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers')
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlerbars', engine())
-app.set('view engine', 'handlers')
-app.set('views,', './views')
+const helpers = require('./utils/helpers');
 
+const hbs = exphbs.create({helpers});
+
+
+const Sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')('session.store');
+
+app.use(session({
+  secret: 'Super secret secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {},
+  store: new SequelizeStore({
+    db: sequelize,
+  })
+}))
+
+
+
+app.engine('handlerbars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/dish-routes'));
 
-app.listen(PORT, () => {
-    console.log('Server listening on: http://localhost:' + PORT);
+sequelize.sync({ force: false}).then(() => {
+    app.listen(PORT, () => console.log("Now listening on port 3001"));
 });
