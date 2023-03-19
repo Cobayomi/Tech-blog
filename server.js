394f7218c426981a7a70f9a09418b58983bae51a
capricia <capriciaw16@gmail.com>
2023-03-18 21:39:57 -0500

    updserver

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const { engine } = ('express-handlerbars')
 const exphbs = require('express-handlebars')
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+app.engine('handlerbars', engine())
+app.set('view engine', 'handlers')
+app.set('views,', './views')
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/dish-routes'));
+
+app.listen(PORT, () => {
+    console.log('Server listening on: http://localhost:' + PORT);
+});
